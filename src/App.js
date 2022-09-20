4c0ff130d7ef5b9587f151beeb6eaad116fb6fa6
junhong91 <junhong199@gmail.com>
2022-09-20 19:55:39 +0900

    Implement FrontEnd Deployment card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import React, { createRef } from "react";
+
+import Deployment from "./components/Deployment";
+
 function App() {
-  return <div className="App">Simple DAPP tutorial :)</div>;
+  const formRef = createRef();
+  const nameInputRef = createRef();
+  const descInputRef = createRef();
+  const DEPLOYMENT = {
+    header: {
+      title: "creaon deploy smart contract on polygon! 😀",
+    },
+    btnName: "Deploy",
+    formRef: formRef,
+    inputItems: [
+      { name: "name", inputRef: nameInputRef },
+      { name: "description", inputRef: descInputRef },
+    ],
+  };
+
+  function onSubmit(event) {
+    event.preventDefault();
+    formRef.current.reset();
+  }
+
+  return (
+    <div>
+      <Deployment deployment={DEPLOYMENT} onSubmit={onSubmit} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Deployment.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import "./Deployment.css";
+
+const Deployment = ({ deployment, onSubmit }) => {
+  return (
+    <div className="deployment__wrapper">
+      <div className="deployment__header">
+        <h1 className="deployment__header__title">{deployment.header.title}</h1>
+      </div>
+      <form className="deployment__form" ref={deployment.formRef} onSubmit={onSubmit}>
+        {deployment.inputItems &&
+          deployment.inputItems.map((item) => (
+            <p key={item.name}>
+              <label className="deployment__label">
+                {item.name}
+                <br />
+                <input
+                  className="deployment__input"
+                  type="text"
+                  ref={item.inputRef}
+                  defaultValue={item?.defaultValue}
+                />
+              </label>
+            </p>
+          ))}
+        <button className="deployment__sign-up__btn">{deployment.btnName}</button>
+      </form>
+    </div>
+  );
+};
+
+export default Deployment;
