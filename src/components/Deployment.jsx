7eed2e0ee616b918a54eff91890ba45d8b9a74c6
junhong91 <junhong199@gmail.com>
2022-09-20 20:11:18 +0900

    [CSS] add background color - light gray

--- src/components/Deployment.jsx
+++ src/components/Deployment.jsx
@@ -4,28 +4,30 @@ import "./Deployment.css";
 
 const Deployment = ({ deployment, onSubmit }) => {
   return (
-    <div className="deployment__wrapper">
-      <div className="deployment__header">
-        <h1 className="deployment__header__title">{deployment.header.title}</h1>
+    <div className="background">
+      <div className="deployment__wrapper">
+        <div className="deployment__header">
+          <h1 className="deployment__header__title">{deployment.header.title}</h1>
+        </div>
+        <form className="deployment__form" ref={deployment.formRef} onSubmit={onSubmit}>
+          {deployment.inputItems &&
+            deployment.inputItems.map((item) => (
+              <p key={item.name}>
+                <label className="deployment__label">
+                  {item.name}
+                  <br />
+                  <input
+                    className="deployment__input"
+                    type="text"
+                    ref={item.inputRef}
+                    defaultValue={item?.defaultValue}
+                  />
+                </label>
+              </p>
+            ))}
+          <button className="deployment__sign-up__btn">{deployment.btnName}</button>
+        </form>
       </div>
-      <form className="deployment__form" ref={deployment.formRef} onSubmit={onSubmit}>
-        {deployment.inputItems &&
-          deployment.inputItems.map((item) => (
-            <p key={item.name}>
-              <label className="deployment__label">
-                {item.name}
-                <br />
-                <input
-                  className="deployment__input"
-                  type="text"
-                  ref={item.inputRef}
-                  defaultValue={item?.defaultValue}
-                />
-              </label>
-            </p>
-          ))}
-        <button className="deployment__sign-up__btn">{deployment.btnName}</button>
-      </form>
     </div>
   );
 };
